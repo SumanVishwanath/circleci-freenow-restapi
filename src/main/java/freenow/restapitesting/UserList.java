caa5d674b5498fdacfa5113588eccd7405814b4d
sumanvishwanath <suman.vishwanath30@gmail.com>
2020-01-02 22:04:53 +0530

    Updated

--- src/main/java/freenow/restapitesting/UserList.java
+++ src/main/java/freenow/restapitesting/UserList.java
@@ -1,60 +1,43 @@
 package freenow.restapitesting;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
-import io.restassured.RestAssured;
+
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import utils.ConfigUtility;
+import utils.GetUrl;
+
 
 public class UserList {
-	public static String postuser, DefinedUsername;
-	public static int UserListJSONSize;
-
-	public static List<String> Users;
 
 	public UserList() {
 		// Defeat instantiation
 	}
 
-	public static Map<String, String> UserLists(String DefinedUsername) {
-
-		Response resp = ApiResponse();
-		Map<String, String> MapOfUsers = getUserListValues(resp);
-		return MapOfUsers;
+	public Response ApiResponse() throws FileNotFoundException, IOException {
+		RequestSpecification httpRequest = GetUrl.getBaseUrl();
+		return httpRequest.get(ConfigUtility.getUserPath());
 
 	}
 
-	public static Response ApiResponse() {
+	public Map<String, String> getUserListValues(String definedUsername1) throws FileNotFoundException, IOException {
+		Map<String, String> userNameAndID = new HashMap<String, String>();
+		JsonPath js = new JsonPath(ApiResponse().asString());
 
-		RequestSpecification httpRequest = RestAssured.given().baseUri("https://jsonplaceholder.typicode.com");
-		Response GETRESPONSE = httpRequest.get("/users");
-		return GETRESPONSE;
-	}
-
-	public static Map<String, String> getUserListValues(Response GETRESPONSE) {
-		String key = GETRESPONSE.asString();
-		JsonPath js = new JsonPath(key);
-
-		List<String> jsonResponse = js.getList("$");
-		UserListJSONSize = jsonResponse.size();
-
-		Map<String, String> jsonResponse1 = new HashMap();
-
-		for (int i = 0; i < UserListJSONSize; i++) {
+		for (int i = 0; i < js.getList("$").size(); i++) {
 			String Username = js.getString("username[" + i + "]");
-			String id = js.getString("id[" + i + "]");
-			// Users = js.getList("username");
-			// String AllUsers = String.valueOf(Users.get(i));
-			DefinedUsername = "Samantha";
-			if (Username.contains(DefinedUsername)) {
-				jsonResponse1.put(Username, id);
-				postuser = id;
-	           System.out.println("UserName and Id Map" + jsonResponse1 + "\n" + "Value of id for entered User is " + postuser);
+			if (Username.contains(definedUsername1)) {
+				String id = js.getString("id[" + i + "]");
+				userNameAndID.put(Username, id);
 			}
 
 		}
-		return jsonResponse1;
+		return userNameAndID;
 	}
+
 }
